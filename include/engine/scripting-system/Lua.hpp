d57460eb30bd0f2a38dae1d442ba48ae48e3f80c
Ghabriel <ghabriel.nunes@gmail.com>
2018-07-29 12:57:48 -0300

    Refactored all Lua-related files

--- include/engine/scripting-system/Lua.hpp
+++ include/engine/scripting-system/Lua.hpp
@@ -1,147 +1,64 @@
 #ifndef SCRIPTING_SYSTEM_LUA_HPP
 #define SCRIPTING_SYSTEM_LUA_HPP
 
+#include <cassert>
 #include <iostream>
 #include <vector>
-
-extern "C" {
-    #include <lua.h>
-    #include <lauxlib.h>
-    #include <lualib.h>
-}
+#include "LuaWrapper.hpp"
 
 namespace engine::scriptingsystem {
     class Lua {
      public:
         Lua(const std::string& filename);
-        ~Lua();
-        void printError(const std::string& variableName, const std::string& reason);
 
         template<typename T>
         T get(const std::string& variableName);
-        bool lua_gettostack(const std::string& variableName);
-
-        // Generic get
-        template<typename T>
-        T lua_get(const std::string& variableName) {
-            return 0;
-        }
-
-        // Generic default get
-        template<typename T>
-        T lua_getdefault() {
-            return 0;
-        }
 
      private:
-        lua_State* L;
+        LuaWrapper luaState;
         int level;
+
+        void pushValueToStack(const std::string& variableName);
     };
 
-    inline Lua::Lua(const std::string& filename) {
-        L = luaL_newstate();
-        if (luaL_loadfile(L, filename.c_str()) || lua_pcall(L, 0, 0, 0)) {
-            std::cout << "Error: script not loaded (" << filename << ")" << std::endl;
-            L = nullptr;
-        }
-    }
-
-    inline Lua::~Lua() {
-        if (L) lua_close(L);
-    }
-
-    void Lua::printError(const std::string& variableName, const std::string& reason) {
-        std::cout << "Error: can't get [" << variableName << "]. " << reason << std::endl;
-    }
+    inline Lua::Lua(const std::string& filename) : luaState(filename) { }
 
     template<typename T>
-    T Lua::get(const std::string& variableName) {
-        if (!L) {
-            printError(variableName, "Script is not loaded");
-            return lua_getdefault<T>();
-        }
-
-        T result;
-        if (lua_gettostack(variableName)) { // variable succesfully on top of stack
-            result = lua_get<T>(variableName);
-        } else {
-            result = lua_getdefault<T>();
-        }
-
-        lua_pop(L, level + 1); // pop all existing elements from stack
+    inline T Lua::get(const std::string& variableName) {
+        pushValueToStack(variableName);
+        T result = luaState.get<T>();
+        luaState.pop(level + 1);
         return result;
     }
 
-    bool Lua::lua_gettostack(const std::string& variableName) {
+    inline void Lua::pushValueToStack(const std::string& variableName) {
         level = 0;
-        std::string var = "";
-        for(unsigned int i = 0; i < variableName.size(); i++) {
-            if (variableName.at(i) == '.') {
-                if (level == 0) {
-                    lua_getglobal(L, var.c_str());
-                } else {
-                    lua_getfield(L, -1, var.c_str());
-                }
+        std::string var;
 
-                if (lua_isnil(L, -1)) {
-                    printError(variableName, var + " is not defined");
-                    return false;
-                } else {
-                    var = "";
-                    level++;
-                }
-            } else {
-                var += variableName.at(i);
+        for (char ch : variableName) {
+            if (ch != '.') {
+                var += ch;
+                continue;
             }
+            
+            if (level == 0) {
+                luaState.pushGlobal(var);
+            } else {
+                luaState.pushField(var);
+            }
+
+            assert(!luaState.isNil());
+            var = "";
+            ++level;
         }
+
         if (level == 0) {
-            lua_getglobal(L, var.c_str());
+            luaState.pushGlobal(var);
         } else {
-            lua_getfield(L, -1, var.c_str());
-        }
-        if (lua_isnil(L, -1)) {
-            printError(variableName, var + " is not defined");
-            return false;
+            luaState.pushField(var);
         }
 
-        return true;
-    }
-
-    template<>
-    std::string Lua::lua_getdefault() {
-        return "null";
-    }
-
-    template <>
-    inline bool Lua::lua_get(const std::string& variableName) {
-        return (bool)lua_toboolean(L, -1);
-    }
-
-    template <>
-    inline float Lua::lua_get(const std::string& variableName) {
-        if (!lua_isnumber(L, -1)) {
-            printError(variableName, "Not a number");
-        }
-        return (float)lua_tonumber(L, -1);
-    }
-
-    template <>
-    inline int Lua::lua_get(const std::string& variableName) {
-        if (!lua_isnumber(L, -1)) {
-            printError(variableName, "Not a number");
-        }
-        return (int)lua_tonumber(L, -1);
-    }
-
-    template <>
-    inline std::string Lua::lua_get(const std::string& variableName) {
-        std::string s = "null";
-        if (lua_isstring(L, -1)) {
-            s = std::string(lua_tostring(L, -1));
-        } else {
-            printError(variableName, "Not a string");
-        }
-        return s;
+        assert(!luaState.isNil());
     }
 }
 
---
+++ include/engine/scripting-system/LuaWrapper.hpp
@@ -0,0 +1,65 @@
+#ifndef SCRIPTING_SYSTEM_LUA_WRAPPER_HPP
+#define SCRIPTING_SYSTEM_LUA_WRAPPER_HPP
+
+#include <cassert>
+#include "LuaRAII.hpp"
+
+namespace engine::scriptingsystem {
+    class LuaWrapper {
+     public:
+        LuaWrapper(const std::string& filename);
+
+        void pushGlobal(const std::string& variableName);
+        void pushField(const std::string& fieldName);
+        void pop(size_t count = 1);
+        bool isNil() const;
+        template<typename T>
+        T get() const;
+
+     private:
+        LuaRAII L;
+    };
+
+    LuaWrapper::LuaWrapper(const std::string& filename) : L(filename) { }
+
+    inline void LuaWrapper::pushGlobal(const std::string& variableName) {
+        lua_getglobal(L.get(), variableName.c_str());
+    }
+
+    inline void LuaWrapper::pushField(const std::string& fieldName) {
+        lua_getfield(L.get(), -1, fieldName.c_str());
+    }
+
+    inline void LuaWrapper::pop(size_t count) {
+        lua_pop(L.get(), count);
+    }
+
+    inline bool LuaWrapper::isNil() const {
+        return lua_isnil(L.get(), -1);
+    }
+
+    template<>
+    inline bool LuaWrapper::get() const {
+        return lua_toboolean(L.get(), -1);
+    }
+
+    template<>
+    inline float LuaWrapper::get() const {
+        assert(lua_isnumber(L.get(), -1));
+        return lua_tonumber(L.get(), -1);
+    }
+
+    template<>
+    inline int LuaWrapper::get() const {
+        assert(lua_isnumber(L.get(), -1));
+        return lua_tonumber(L.get(), -1);
+    }
+
+    template<>
+    inline std::string LuaWrapper::get() const {
+        assert(lua_isstring(L.get(), -1));
+        return std::string(lua_tostring(L.get(), -1));
+    }
+}
+
+#endif
